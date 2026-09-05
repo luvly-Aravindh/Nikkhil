2aedab16a6d31603ac7b4a4b994df63a02d662e9
Aravindh. E <87863243+luvly-Aravindh@users.noreply.github.com>
2026-09-05 12:10:58 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-<<<<<<< HEAD
-import "./styles/original.css";
-=======
->>>>>>> f4425e905dc47b20d52c27d8072a45cbab91bba0
+
 ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode><App /></React.StrictMode>);
